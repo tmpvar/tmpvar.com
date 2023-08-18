65ceec6c5d3467b23a7a3fc32b3b298957612ace
Elijah Insua <tmpvar@gmail.com>
2023-08-18 16:38:14 -0700

    reverse proxy for posthog

---
+++ functions/api/userdata.js
@@ -0,0 +1,30 @@
+const API_HOST = "app.posthog.com" // Change to "eu.posthog.com" for the EU region
+
+async function handleRequest(event) {
+    const pathname = new URL(event.request.url).pathname
+    if (pathname.startsWith("/static/")) {
+        return retrieveStatic(event, pathname)
+    } else {
+        return forwardRequest(event, pathname)
+    }
+}
+
+async function retrieveStatic(event, pathname) {
+    let response = await caches.default.match(event.request)
+    if (!response) {
+        response = await fetch(`https://${API_HOST}${pathname}`)
+        event.waitUntil(caches.default.put(event.request, response.clone()))
+    }
+    return response
+}
+
+async function forwardRequest(event, pathname) {
+    const request = new Request(event.request)
+    request.headers.delete("cookie")
+    return await fetch(`https://${API_HOST}${pathname}`, request)
+}
+
+addEventListener("fetch", (event) => {
+    event.passThroughOnException()
+    event.respondWith(handleRequest(event))
+})
