8cd7d55b73d5de9177aea31de7d6ded45d559766
Elijah Insua <tmpvar@gmail.com>
2023-08-18 16:52:59 -0700

    move stuff around

--- functions/api/userdata/[[catchall]].js
+++ functions/api/userdata/[[catchall]].js
@@ -1,8 +1,12 @@
 const API_HOST = "app.posthog.com" // Change to "eu.posthog.com" for the EU region
 
+export function onRequest(context) {
+  return new Response(JSON.stringify(context.params.catchall))
+}
+
 async function handleRequest(event) {
     const pathname = (new URL(event.request.url).pathname).replace("/api/userdata", "");
-    console.log("pathname:", pathname)
+    console.log("url:", event.request.url, "catchall:", context.params.catchall)
     if (pathname.startsWith("/static/")) {
         return retrieveStatic(event, pathname)
     } else {
@@ -25,7 +29,7 @@ async function forwardRequest(event, pathname) {
     return await fetch(`https://${API_HOST}${pathname}`, request)
 }
 
-addEventListener("fetch", (event) => {
-    event.passThroughOnException()
-    event.respondWith(handleRequest(event))
-})
+// addEventListener("fetch", (event) => {
+//     event.passThroughOnException()
+//     event.respondWith(handleRequest(event))
+// })
