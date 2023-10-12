2987f31b20d62f8171b23aec477fb03e03af4797
Elijah Insua <tmpvar@gmail.com>
2023-10-12 13:38:13 -0700

    begin work on brute force screen space ray marching

--- content/poc/radiance-cascades/screen-space-3d/screen-space-3d.js
+++ content/poc/radiance-cascades/screen-space-3d/screen-space-3d.js
@@ -257,9 +257,9 @@ async function ScreenSpace3DBegin(rootEl) {
   const WGSLObjectDataStruct = /* wgsl */`
     struct ObjectData {
       // props: mat4x4<f32>,
-      // // rgb, a=objectType
+      // rgb, a=objectType
       albedo: vec4f,
-      // // rgb, a=unused
+      // rgb, a=unused
       emission: vec4f,
       transform: mat4x4<f32>,
     };
@@ -646,13 +646,274 @@ async function ScreenSpace3DBegin(rootEl) {
       }
     },
 
-    ScreenSpaceBruteForce(gpu, fluenceTexture, depthTexture, objectIDTexture) {
+    ScreenSpaceBruteForce(
+      gpu,
+      fluencePreviousTexture,
+      fluenceCurrentTexture,
+      depthTexture,
+      objectIDTexture,
+      objectBuffer,
+      workgroupSize,
+      raysPerPixel,
+    ) {
+      const labelPrefix = `${gpu.labelPrefix}ScreenSpaceBruteForce/`
+      const sampler = gpu.device.createSampler({
+        label: `${labelPrefix}Sampler`,
+        addressModeU: 'clamp-to-edge',
+        addressModeV: 'clamp-to-edge',
+        magFilter: 'linear',
+        minFilter: 'linear',
+        mipmapFilter: 'linear',
+      })
+
+      const source =  /* wgsl */`
+        const PI: f32 = ${Math.PI};
+        const TAU: f32 = ${Math.PI * 2.0};
+
+        fn LinearizeDepth(depth: f32) -> f32{
+          let zNear = 0.2; // TODO: Replace by the zNear of your perspective projection
+          let zFar = 20.0; // TODO: Replace by the zFar  of your perspective projection
+
+          return (2.0 * zNear) / (zFar + zNear - depth * (zFar - zNear));
+        }
+
+        fn JetLinearRescale(u: f32, v: f32, x: f32) -> f32 {
+          return (x - u) / (v - u);
+        }
+
+        fn JetLinear(t: f32) -> vec3f {
+          const color0 = vec3f(0.0, 0.0, 0.5625); // blue
+          const u1 = 1.0 / 9.0;
+          const color1 = vec3f(0.0, 0.0, 1.0); // blue
+          const u2 = 23.0 / 63.0;
+          const color2 = vec3f(0.0, 1.0, 1.0); // cyan
+          const u3 = 13.0 / 21.0;
+          const color3 = vec3f(1.0, 1.0, 0.0); // yellow
+          const u4 = 47.0 / 63.0;
+          const color4 = vec3f(1.0, 0.5, 0.0); // orange
+          const u5 = 55.0 / 63.0;
+          const color5 = vec3f(1.0, 0.0, 0.0); // red
+          const color6 = vec3(0.5, 0.0, 0.0); // red
+          return mix(color0, color1, JetLinearRescale(0.0, u1, t)) +
+                (mix(color1, color2, JetLinearRescale(u1, u2, t)) -
+                  mix(color0, color1, JetLinearRescale(0.0, u1, t))) *
+                  step(u1, t) +
+                (mix(color2, color3, JetLinearRescale(u2, u3, t)) -
+                  mix(color1, color2, JetLinearRescale(u1, u2, t))) *
+                  step(u2, t) +
+                (mix(color3, color4, JetLinearRescale(u3, u4, t)) -
+                  mix(color2, color3, JetLinearRescale(u2, u3, t))) *
+                  step(u3, t) +
+                (mix(color4, color5, JetLinearRescale(u4, u5, t)) -
+                  mix(color3, color4, JetLinearRescale(u3, u4, t))) *
+                  step(u4, t) +
+                (mix(color5, color6, JetLinearRescale(u5, 1.0, t)) -
+                  mix(color4, color5, JetLinearRescale(u4, u5, t))) *
+                  step(u5, t);
+        }
+
+        ${WGSLObjectDataStruct}
+
+        @group(0) @binding(0) var fluenceWriteTexture: texture_storage_2d<rgba32float, write>;
+        @group(0) @binding(1) var fluenceReadTexture: texture_2d<f32>;
+        @group(0) @binding(2) var depthTexture: texture_depth_2d;
+        @group(0) @binding(3) var depthSampler: sampler;
+        @group(0) @binding(4) var objectIDTexture: texture_2d<u32>;
+        @group(0) @binding(5) var<storage, read_write> objectData: array<ObjectData>;
+        @compute @workgroup_size(${workgroupSize.join(',')})
+        fn ComputeMain(@builtin(global_invocation_id) id: vec3<u32>) {
+          let dims = vec2f(textureDimensions(depthTexture));
+          let invDims = 1.0 / dims;
+          let uv = vec2f(id.xy) * invDims;
+          let startingDepth = textureSampleLevel(depthTexture, depthSampler, uv, 0);
+
+          // textureStore(fluenceWriteTexture, id.xy, vec4(startingDepth, 0.0, 0.0, 1.0));
+          // return;
+
+          if (startingDepth >= 1.0) {
+            textureStore(fluenceWriteTexture, id.xy, vec4(1.0, 0.0, 1.0, 1.0));
+            return;
+          }
+          let startingLinearDepth = LinearizeDepth(startingDepth);
 
 
+          var fluence = vec3f(0.0);// = textureLoad(fluenceReadTexture, id.xy, 0);
+
+          // textureStore(fluenceWriteTexture, id.xy, vec4(uv, 0.0, 1.0));
+          // textureStore(fluenceWriteTexture, id.xy, vec4(startingDepth, 0.0, 0.0, 1.0));
+          let rayCount = 16.0;
+          let angleStep = TAU / rayCount;
+          let thickness = 10.5;
+          var hits = 0.0;
+          for (var angle=0.0; angle<TAU; angle+=angleStep) {
+            var direction = vec2f(cos(angle), sin(angle)) * invDims;
+            var steps = 256;
+            var sampleUV = uv;
+            var occlusion = 0.0;
+            while(steps > 0) {
+              steps--;
+
+              sampleUV += direction;
+              let depth = LinearizeDepth(
+                textureSampleLevel(depthTexture, depthSampler, sampleUV, 0)
+              );
+
+              let objectID = textureLoad(objectIDTexture, vec2<u32>(dims * sampleUV), 0).r;
+              if (objectID < 0xFFFF) {
+                let depthDelta = depth - startingLinearDepth;
+                if (depthDelta > 0.0 && depthDelta < thickness) {
+                  fluence += objectData[objectID].emission.rgb;
+                  break;
+                }
+
+                if (objectID < 0xFFFF) {
+                  hits += 1.0;
+                  fluence += objectData[objectID].emission.rgb;
+
+                }
+
+              }
+            }
+          }
+
+          if (hits > 0.0) {
+            fluence /= hits;
+          }
 
 
-      return function ScreenSpaceBruteForce() {
+          // let linearDepth = LinearizeDepth(startingDepth);
+          // textureStore(fluenceWriteTexture, id.xy, vec4(JetLinear(linearDepth), 1.0));
 
+          textureStore(fluenceWriteTexture, id.xy, vec4(fluence, 1.0));
+        }
+      `
+
+      const shaderModule = gpu.device.createShaderModule({
+        label: `${labelPrefix}ShaderModule`,
+        code: source
+      })
+
+      const bindGroupLayout = gpu.device.createBindGroupLayout({
+        label: `${labelPrefix}BindGroupLayout`,
+        entries: [
+          {
+            binding: 0,
+            visibility: GPUShaderStage.COMPUTE,
+            storageTexture: {
+              format: "rgba32float"
+            },
+          },
+          {
+            binding: 1,
+            visibility: GPUShaderStage.COMPUTE,
+            texture: {
+              sampleType: 'unfilterable-float',
+            },
+          },
+          {
+            binding: 2,
+            visibility: GPUShaderStage.COMPUTE,
+            texture: {
+              sampleType: 'depth',
+            },
+          },
+          {
+            binding: 3,
+            visibility: GPUShaderStage.COMPUTE,
+            sampler: {
+              type: "filtering"
+            },
+          },
+          {
+            binding: 4,
+            visibility: GPUShaderStage.COMPUTE,
+            texture: {
+              sampleType: 'uint',
+              viewDimension: '2d',
+            },
+          },
+          {
+            binding: 5,
+            visibility: GPUShaderStage.COMPUTE,
+            buffer: {
+              type: 'storage'
+            },
+          },
+        ]
+      })
+
+      const pipeline = gpu.device.createComputePipeline({
+        label: `${labelPrefix}ComputePipeline`,
+        compute: {
+          module: shaderModule,
+          entryPoint: 'ComputeMain',
+        },
+        layout: gpu.device.createPipelineLayout({
+          bindGroupLayouts: [
+            bindGroupLayout
+          ]
+        }),
+      })
+
+      const bindGroup = gpu.device.createBindGroup({
+        label: `${labelPrefix}BindGroup`,
+        layout: pipeline.getBindGroupLayout(0),
+        entries: [
+          {
+            binding: 0,
+            resource: fluenceCurrentTexture.createView(),
+          },
+          {
+            binding: 1,
+            resource: fluencePreviousTexture.createView(),
+          },
+          {
+            binding: 2,
+            resource: depthTexture.createView({
+              dimension: '2d',
+              baseMipLevel: 0,
+              mipLevelCount: depthTexture.mipLevelCount,
+              aspect: 'depth-only',
+            })
+          },
+          {
+            binding: 3,
+            resource: sampler,
+          },
+          {
+            binding: 4,
+            resource: objectIDTexture.createView()
+          },
+          {
+            binding: 5,
+            resource: {
+              buffer: objectBuffer
+            }
+          },
+        ]
+      })
+
+
+      return function ScreenSpaceBruteForce(commandEncoder) {
+        commandEncoder.copyTextureToTexture(
+          { texture: fluenceCurrentTexture },
+          { texture: fluencePreviousTexture },
+          [
+            fluenceCurrentTexture.width,
+            fluenceCurrentTexture.height,
+            1
+          ]
+        )
+
+        const computePass = commandEncoder.beginComputePass()
+        computePass.setPipeline(pipeline)
+        computePass.setBindGroup(0, bindGroup)
+        computePass.dispatchWorkgroups(
+          Math.floor(depthTexture.width / workgroupSize[0] + 1),
+          Math.floor(depthTexture.height / workgroupSize[1] + 1),
+          1
+        )
+        computePass.end()
       }
     }
 
@@ -663,7 +924,11 @@ async function ScreenSpace3DBegin(rootEl) {
       label: `${state.gpu.labelPrefix}DepthTexture/`,
       size: [canvas.width, canvas.height],
       dimension: '2d',
-      usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC,
+      usage: (
+        GPUTextureUsage.RENDER_ATTACHMENT |
+        GPUTextureUsage.TEXTURE_BINDING |
+        GPUTextureUsage.COPY_SRC
+      ),
       format: 'depth24plus-stencil8'
     }),
 
@@ -678,11 +943,28 @@ async function ScreenSpace3DBegin(rootEl) {
 
     }),
 
-    fluence: state.gpu.device.createTexture({
-      label: `${state.gpu.labelPrefix}Fluence/`,
+    fluenceCurrent: state.gpu.device.createTexture({
+      label: `${state.gpu.labelPrefix}Fluence/Current`,
       size: [canvas.width, canvas.height],
       dimension: '2d',
-      usage: GPUTextureUsage.COPY_SRC | GPUTextureUsage.TEXTURE_BINDING,
+      usage: (
+        GPUTextureUsage.COPY_SRC |
+        GPUTextureUsage.TEXTURE_BINDING |
+        GPUTextureUsage.STORAGE_BINDING
+      ),
+      format: 'rgba32float',
+    }),
+
+    fluencePrevious: state.gpu.device.createTexture({
+      label: `${state.gpu.labelPrefix}Fluence/Previous`,
+      size: [canvas.width, canvas.height],
+      dimension: '2d',
+      usage: (
+        GPUTextureUsage.COPY_SRC |
+        GPUTextureUsage.COPY_DST |
+        GPUTextureUsage.TEXTURE_BINDING |
+        GPUTextureUsage.STORAGE_BINDING
+      ),
       format: 'rgba32float',
     }),
   }
@@ -760,9 +1042,19 @@ async function ScreenSpace3DBegin(rootEl) {
     ),
     debugFluence: shaders.DebugBlitFluence(
       state.gpu,
-      textures.fluence,
+      textures.fluenceCurrent,
       state.gpu.buffers.objects
-    )
+    ),
+    screenSpaceBruteForce: shaders.ScreenSpaceBruteForce(
+      state.gpu,
+      textures.fluencePrevious,
+      textures.fluenceCurrent,
+      textures.depth,
+      textures.objectID,
+      state.gpu.buffers.objects,
+      [16, 16, 1],
+      8// raysPerPixel
+    ),
   }
 
   const scenes = {}
@@ -960,6 +1252,10 @@ async function ScreenSpace3DBegin(rootEl) {
       })
 
       pass.end();
+    }
+
+    {
+      programs.screenSpaceBruteForce(commandEncoder);
     }
 
     if (state.params.debugRenderObjectIDBuffer) {
