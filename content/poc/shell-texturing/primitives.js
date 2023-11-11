7da1cea58663105ab9fa206eeb156a26505cda40
Elijah Insua <tmpvar@gmail.com>
2023-11-10 17:54:31 -0700

    add Shell Texturing POC

---
+++ content/poc/shell-texturing/primitives.js
@@ -0,0 +1,249 @@
+import GenerateIcosphere from "./icosphere-fast.js"
+export function CreatePlane(gpu) {
+  const labelPrefix = `${gpu.labelPrefix}Primitive/Plane/`
+
+  let positions = new Float32Array([
+    // front
+    1, -1, 0,
+    -1, -1, 0,
+    -1, 1, 0,
+
+    1, 1, 0,
+    1, -1, 0,
+    -1, 1, 0,
+  ])
+
+
+  let normals = new Float32Array([
+    // front
+    0, 0, -1,
+    0, 0, -1,
+    0, 0, -1,
+
+    0, 0, -1,
+    0, 0, -1,
+    0, 0, -1,
+  ])
+
+  const mesh = {
+    label: labelPrefix,
+    vertexCount: positions.length / 3,
+    positions: positions,
+    normals: normals,
+  }
+
+  mesh.positionBuffer = gpu.device.createBuffer({
+    label: `${labelPrefix}PositionBuffer`,
+    size: positions.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+  })
+
+  mesh.normalBuffer = gpu.device.createBuffer({
+    label: `${labelPrefix}NormalBuffer`,
+    size: normals.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+  })
+
+  gpu.device.queue.writeBuffer(mesh.positionBuffer, 0, mesh.positions)
+  gpu.device.queue.writeBuffer(mesh.normalBuffer, 0, mesh.normals)
+
+  return mesh
+}
+
+
+
+export function CreateCube(gpu) {
+  const labelPrefix = `${gpu.labelPrefix}Primitive/Cube/`
+
+  let positions = new Float32Array([
+    // front
+    -1, -1, -1,
+    1, -1, -1,
+    -1, 1, -1,
+
+    1, -1, -1,
+    1, 1, -1,
+    -1, 1, -1,
+
+    // back
+    1, -1, 1,
+    -1, -1, 1,
+    -1, 1, 1,
+
+    1, 1, 1,
+    1, -1, 1,
+    -1, 1, 1,
+
+    // top
+    -1, 1, -1,
+    1, 1, -1,
+    -1, 1, 1,
+
+    1, 1, -1,
+    1, 1, 1,
+    -1, 1, 1,
+
+    // bottom
+    1, -1, -1,
+    -1, -1, -1,
+    -1, -1, 1,
+
+    1, -1, 1,
+    1, -1, -1,
+    -1, -1, 1,
+
+    // left
+    -1, -1, -1,
+    -1, 1, -1,
+    -1, -1, 1,
+
+    -1, 1, -1,
+    -1, 1, 1,
+    -1, -1, 1,
+
+    // right
+    1, 1, -1,
+    1, -1, -1,
+    1, -1, 1,
+
+    1, 1, 1,
+    1, 1, -1,
+    1, -1, 1,
+
+
+  ])
+
+
+  let normals = new Float32Array([
+    // front
+    0, 0, -1,
+    0, 0, -1,
+    0, 0, -1,
+
+    0, 0, -1,
+    0, 0, -1,
+    0, 0, -1,
+
+    // back
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1,
+
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1,
+
+    // top
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0,
+
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0,
+
+    // bottom
+    0, -1, 0,
+    0, -1, 0,
+    0, -1, 0,
+
+    0, -1, 0,
+    0, -1, 0,
+    0, -1, 0,
+
+    // left
+    -1, 0, 0,
+    -1, 0, 0,
+    -1, 0, 0,
+
+    -1, 0, 0,
+    -1, 0, 0,
+    -1, 0, 0,
+
+    // right
+    1, 0, 0,
+    1, 0, 0,
+    1, 0, 0,
+
+    1, 0, 0,
+    1, 0, 0,
+    1, 0, 0,
+  ])
+
+  const mesh = {
+    label: labelPrefix,
+    vertexCount: positions.length / 3,
+    positions: positions,
+    normals: normals,
+  }
+
+  mesh.positionBuffer = gpu.device.createBuffer({
+    label: `${labelPrefix}PositionBuffer`,
+    size: positions.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+  })
+
+  mesh.normalBuffer = gpu.device.createBuffer({
+    label: `${labelPrefix}NormalBuffer`,
+    size: normals.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+  })
+
+  gpu.device.queue.writeBuffer(mesh.positionBuffer, 0, mesh.positions)
+  gpu.device.queue.writeBuffer(mesh.normalBuffer, 0, mesh.normals)
+
+  return mesh
+}
+
+export function CreateSphere(gpu, subdivisions) {
+  const labelPrefix = `${gpu.label}Primitive/Sphere`
+  const sphere = GenerateIcosphere(subdivisions)
+
+  let positions = new Float32Array(sphere.cells.length * 3 * 3)
+  let normals = new Float32Array(sphere.cells.length * 3 * 3)
+
+  // build soup out of cells / positions
+  sphere.cells.forEach((cell, i) => {
+    cell.forEach((vertIndex, j) => {
+      let a = sphere.positions[vertIndex][0]
+      let b = sphere.positions[vertIndex][1]
+      let c = sphere.positions[vertIndex][2]
+
+      let l = Math.sqrt(a * a + b * b + c * c)
+
+      let outIndex = i * 9 + j * 3
+
+      positions[outIndex + 1] = a
+      positions[outIndex + 0] = b
+      positions[outIndex + 2] = c
+
+      normals[outIndex + 1] = a / l
+      normals[outIndex + 0] = b / l
+      normals[outIndex + 2] = c / l
+    })
+  })
+
+  const mesh = {
+    label: labelPrefix,
+    vertexCount: positions.length / 3,
+    positions: positions,
+    normals: normals,
+  }
+
+  mesh.positionBuffer = gpu.device.createBuffer({
+    label: `${labelPrefix}PositionBuffer`,
+    size: positions.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+  })
+
+  mesh.normalBuffer = gpu.device.createBuffer({
+    label: `${labelPrefix}NormalBuffer`,
+    size: normals.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+  })
+
+  gpu.device.queue.writeBuffer(mesh.positionBuffer, 0, mesh.positions)
+  gpu.device.queue.writeBuffer(mesh.normalBuffer, 0, mesh.normals)
+
+  return mesh
+}
---
+++ content/poc/shell-texturing/shell-texturing.js
@@ -0,0 +1,387 @@
+import CreateOrbitCamera from "./orbit-camera.js"
+import CreateParamReader from "./params.js"
+import * as primitives from "./primitives.js"
+
+ShellTexturingBegin(
+  document.getElementById('shell-texturing-content')
+)
+
+async function ShellTexturingBegin(rootEl) {
+  const controlEl = rootEl.querySelector('.controls')
+  const canvas = rootEl.querySelector('canvas')
+
+  function Now() {
+    if (window.performance && window.performance.now) {
+      return window.performance.now()
+    } else {
+      return Time.now()
+    }
+  }
+
+  async function InitGPU(ctx) {
+    let adapter = await navigator.gpu.requestAdapter()
+    let device = await adapter.requestDevice()
+    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+    ctx.configure({
+      device,
+      format: presentationFormat,
+      alphaMode: 'opaque',
+      usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC,
+    });
+
+    return {
+      adapter,
+      device,
+      presentationFormat,
+    }
+  }
+
+  let ctx = canvas.getContext('webgpu')
+
+  let state = {
+    canvas: canvas,
+    ctx,
+    camera: CreateOrbitCamera(),
+    mouse: {
+      pos: [0, 0],
+      lastPos: [0, 0],
+      down: false
+    },
+    lastFrameTime: Now(),
+    params: {},
+    dirty: true,
+  }
+
+  try {
+    state.gpu = await InitGPU(state.ctx)
+    state.gpu.labelPrefix = "ShellTexturing/"
+  } catch (e) {
+    console.log(e)
+    rootEl.className = rootEl.className.replace('has-webgpu', '')
+    return;
+  }
+
+  // Wire up mouse
+  {
+    const MoveMouse = (x, y) => {
+      let ratioX = canvas.width / canvas.clientWidth
+      let ratioY = canvas.height / canvas.clientHeight
+      state.mouse.pos[0] = x * ratioX
+      state.mouse.pos[1] = y * ratioY
+      state.dirty = true;
+    }
+
+    window.addEventListener("mouseup", e => {
+      state.mouse.down = false
+    })
+
+    canvas.addEventListener("mousedown", (e) => {
+      state.mouse.down = true
+      MoveMouse(e.offsetX, e.offsetY);
+      state.mouse.lastPos[0] = state.mouse.pos[0]
+      state.mouse.lastPos[1] = state.mouse.pos[1]
+
+      e.preventDefault()
+    }, { passive: false })
+
+    canvas.addEventListener("mousemove", e => {
+      MoveMouse(e.offsetX, e.offsetY)
+      e.preventDefault()
+
+      if (state.mouse.down) {
+        let dx = state.mouse.pos[0] - state.mouse.lastPos[0]
+        let dy = state.mouse.pos[1] - state.mouse.lastPos[1]
+
+        state.mouse.lastPos[0] = state.mouse.pos[0]
+        state.mouse.lastPos[1] = state.mouse.pos[1]
+
+        if (Math.abs(dx) < 1.0 && Math.abs(dy) < 1.0) {
+          return;
+        }
+
+        state.camera.rotate(dx, -dy)
+      }
+    }, { passive: false })
+
+    canvas.addEventListener("wheel", e => {
+      state.camera.zoom(e.deltaY)
+      state.dirty = true
+      e.preventDefault()
+    }, { passive: false })
+  }
+
+  const textures = {
+    depth: state.gpu.device.createTexture({
+      label: `${state.gpu.labelPrefix}DepthTexture/`,
+      size: [canvas.width, canvas.height],
+      dimension: '2d',
+      usage: (
+        GPUTextureUsage.RENDER_ATTACHMENT |
+        GPUTextureUsage.TEXTURE_BINDING |
+        GPUTextureUsage.COPY_SRC
+      ),
+      format: 'depth24plus-stencil8'
+    }),
+  }
+
+  const shaders = {
+    RenderTriangleSoup(
+      gpu,
+      mesh,
+      depthTexture,
+      presentationFormat
+    ) {
+      const labelPrefix = gpu.labelPrefix + 'RenderMesh/'
+      const device = gpu.device
+      const uboFields = [
+        ['eye', 'vec4f', 16],
+        // x = offset amount, yzw = unused
+        ['params', 'vec4f', 16],
+        ['worldToScreen', 'mat4x4<f32>', 16 * 4],
+      ]
+
+      let uboBufferSize = uboFields.reduce((p, c) => {
+        return p + c[2]
+      }, 0)
+      uboBufferSize = Math.floor(uboBufferSize / 16 + 1) * 16
+      const uboBuffer = new ArrayBuffer(uboBufferSize)
+      const uboData = new DataView(uboBuffer)
+      const ubo = device.createBuffer({
+        label: `${labelPrefix}UBO`,
+        size: uboBuffer.byteLength,
+        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+      })
+
+      const source = /* wgsl */`
+        struct VertexOut {
+          @builtin(position) position : vec4f,
+          @location(0) color: vec3f,
+          @location(1) worldPosition: vec3f,
+        }
+
+        struct UBOParams {
+          ${uboFields.map(v => `${v[0]}: ${v[1]},`).join('\n          ')}
+        };
+
+        @group(0) @binding(0) var<uniform> ubo: UBOParams;
+
+        @vertex
+        fn VertexMain(
+          @location(0) inPosition: vec3f,
+          @location(1) inNormal: vec3f,
+          @builtin(instance_index) instanceIndex: u32
+        ) -> VertexOut {
+          var out: VertexOut;
+
+          out.worldPosition = inPosition - ubo.eye.xyz;
+          out.position = ubo.worldToScreen * vec4(inPosition, 1.0);
+
+          return out;
+        }
+
+        struct FragmentOut {
+          @location(0) color: vec4f,
+        };
+
+        @fragment
+        fn FragmentMain(fragData: VertexOut) -> FragmentOut {
+          var out: FragmentOut;
+          let dFdxPos = dpdx(fragData.worldPosition);
+          let dFdyPos = -dpdy(fragData.worldPosition);
+          let normal = normalize(cross(dFdxPos, dFdyPos));
+          out.color = vec4(normal * 0.5 + 0.5, 1.0);
+          // out.color = vec4(fragData.worldPosition * 0.5 + 0.5, 1.0);
+          return out;
+        }
+      `
+
+      const shaderModule = gpu.device.createShaderModule({
+        label: `${labelPrefix}ShaderModule`,
+        code: source,
+      })
+
+      const bindGroupLayout = gpu.device.createBindGroupLayout({
+        label: `${labelPrefix}BindGroupLayout`,
+        entries: [
+          {
+            binding: 0,
+            visibility: GPUShaderStage.VERTEX,
+            buffer: {
+              type: 'uniform',
+            }
+          },
+        ]
+      })
+
+      const pipeline = device.createRenderPipeline({
+        label: `${labelPrefix}RenderPipeline`,
+        vertex: {
+          module: shaderModule,
+          entryPoint: 'VertexMain',
+          buffers: [
+            // position
+            {
+              attributes: [{
+                shaderLocation: 0,
+                offset: 0,
+                format: 'float32x3',
+              }],
+              arrayStride: 12,
+              stepMode: 'vertex',
+            },
+            // normal
+            {
+              attributes: [{
+                shaderLocation: 1,
+                offset: 0,
+                format: 'float32x3',
+              }],
+              arrayStride: 12,
+              stepMode: 'vertex'
+            },
+          ]
+        },
+        fragment: {
+          module: shaderModule,
+          entryPoint: 'FragmentMain',
+          targets: [{
+            format: presentationFormat,
+          }]
+        },
+        primitive: {
+          topology: 'triangle-list',
+          cullMode: 'none',
+          frontFace: 'cw',
+        },
+        depthStencil: {
+          depthWriteEnabled: true,
+          depthCompare: 'less',
+          format: depthTexture.format,
+        },
+        layout: device.createPipelineLayout({
+          bindGroupLayouts: [
+            bindGroupLayout
+          ]
+        })
+      })
+
+      const bindGroup = device.createBindGroup({
+        label: `${labelPrefix}BindGroup`,
+        layout: pipeline.getBindGroupLayout(0),
+        entries: [
+          {
+            binding: 0,
+            resource: {
+              buffer: ubo
+            }
+          },
+        ]
+      })
+
+      return function RenderMesh(pass, worldToScreen, eye, shellOffset, instanceCount) {
+        // update the uniform buffer
+        {
+          let byteOffset = 0
+
+          eye.forEach(v => {
+            uboData.setFloat32(byteOffset, v, true)
+            byteOffset += 4;
+          })
+          byteOffset += 4
+
+          // params
+          uboData.setFloat32(byteOffset, shellOffset, true)
+          byteOffset += 16
+
+          worldToScreen.forEach(v => {
+            uboData.setFloat32(byteOffset, v, true)
+            byteOffset += 4;
+          })
+
+          gpu.device.queue.writeBuffer(ubo, 0, uboBuffer)
+        }
+
+        pass.setPipeline(pipeline);
+        pass.setBindGroup(0, bindGroup)
+        pass.setVertexBuffer(0, mesh.positionBuffer);
+        pass.setVertexBuffer(1, mesh.normalBuffer);
+        pass.draw(mesh.vertexCount, instanceCount);
+      }
+    },
+  }
+
+  const programs = {
+    renderMesh: shaders.RenderTriangleSoup(
+      state.gpu,
+      primitives.CreatePlane(state.gpu),
+      textures.depth,
+      state.gpu.presentationFormat
+    )
+  }
+
+  function ReadParams() {
+
+  }
+
+  function RenderFrame() {
+    ReadParams()
+
+    const now = Now()
+    const deltaTime = (now - state.lastFrameTime) / 1000.0
+    state.lastFrameTime = now
+
+    if (state.camera.tick(canvas.width, canvas.height, deltaTime)) {
+      state.dirty = true;
+    }
+
+    if (!state.dirty) {
+      requestAnimationFrame(RenderFrame)
+      return
+    }
+
+    let frameTextureView = state.ctx.getCurrentTexture().createView()
+    const commandEncoder = state.gpu.device.createCommandEncoder()
+
+    let colorAttachment = {
+      view: frameTextureView,
+      clearValue: { r: 0, g: 0, b: 0, a: 0 },
+      loadOp: 'clear',
+      storeOp: 'store'
+    };
+
+    let depthAttachment = {
+      view: textures.depth.createView(),
+      depthClearValue: 1,
+      depthLoadOp: 'clear',
+      depthStoreOp: 'store',
+      stencilClearValue: 0,
+      stencilLoadOp: 'clear',
+      stencilStoreOp: 'store',
+    }
+
+    let pass = commandEncoder.beginRenderPass({
+      colorAttachments: [
+        colorAttachment,
+      ],
+      depthStencilAttachment: depthAttachment,
+    });
+
+    let width = canvas.width
+    let height = canvas.height
+    pass.setViewport(0, 0, width, height, 0, 1);
+    pass.setScissorRect(0, 0, width, height);
+    programs.renderMesh(
+      pass,
+      state.camera.computed.worldToScreen,
+      state.camera.computed.eye,
+      1.0,
+      1
+    )
+
+    pass.end();
+    state.gpu.device.queue.submit([commandEncoder.finish()])
+    requestAnimationFrame(RenderFrame)
+  }
+
+  requestAnimationFrame(RenderFrame)
+}
