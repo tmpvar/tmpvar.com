dcf625dcab6ab4154ca5f19018e28ad38aaef27d
Elijah Insua <tmpvar@gmail.com>
2024-03-06 23:26:29 -0700

    a demo of the detector

--- content/poc/hassenstein-reichardt-detector/demo.js
+++ content/poc/hassenstein-reichardt-detector/demo.js
@@ -1,9 +1,10 @@
 Init(document.getElementById('demo-content'))
 
 function CreateReceptor(x, y) {
-  const MaxSamples = 1 << 10
+  const MaxSamples = 1 << 3
   const SampleRate = 100
   const LPCutoff = 1
+  const Radius = 10
 
   return {
     pos: [x, y],
@@ -28,22 +29,93 @@ function CreateReceptor(x, y) {
       const start = Math.max(0, this.head - MaxSamples) % MaxSamples
       for (let i = 0; i < MaxSamples; i++) {
         const index = start + i
-        value = last + alpha * (this.ring[index] - last);
+        value = value + alpha * (this.ring[index] - value);
       }
       return value;
     },
     render(ctx) {
       ctx.save()
       const value = this.getRawValue()
-      console.log(value)
-      ctx.fillStyle = value > 0 ? "green" : "red"
+      const grad = value * 255
+      const gradColor = `rgb(${grad},${grad},${grad})`
+      ctx.fillStyle = gradColor
+      ctx.strokeStyle = gradColor
       ctx.beginPath()
-      ctx.arc(this.pos[0], this.pos[1], 10, 0, TAU)
+      ctx.arc(this.pos[0], this.pos[1], Radius, 0, TAU)
       ctx.fill()
+
+      const start = Math.max(0, this.head - MaxSamples)
+      const end = this.head
+      const width = 100
+      const height = 50
+      const xstep = width / MaxSamples
+      let xoff = this.pos[0] - width / 2
+      let yoff = this.pos[1] + height + Radius
+
+
+      // ctx.fillStyle = "rgba(255, 255, 255, 0.2)"
+      // ctx.strokeStyle = "#000"
+      // ctx.fillRect(xoff, yoff - height, width, height)
+
+      // for (let i = start; i < end; i++) {
+      //   const value = this.ring[i % MaxSamples]
+      //   const valueGrad = value * 255
+      //   const valueColor = `rgb(${valueGrad},${valueGrad},${valueGrad})`
+      //   ctx.beginPath()
+      //   ctx.strokeStyle = valueColor
+      //   ctx.moveTo(xoff, yoff)
+      //   ctx.lineTo(xoff, yoff - value * (height - 5))
+      //   ctx.stroke()
+      //   xoff += xstep;
+      // }
+
       ctx.restore()
     }
   }
+}
 
+function CreateReceptorPair(a, b) {
+  const MaxSamples = 1 << 6
+  return {
+    head: 0,
+    ring: new Float32Array(MaxSamples),
+
+    tick() {
+      const bside = a.getRawValue() * b.getFilteredValue()
+      const aside = b.getRawValue() * a.getFilteredValue()
+      const sample = aside - bside
+
+      const id = this.head++
+      const index = id % MaxSamples
+      const phasor = id * 0.01
+      const sine = Math.sin((phasor - Math.floor(phasor)) * TAU) * 0.2 + 0.2
+
+      this.ring[index] = sample;
+    },
+    render(ctx) {
+      const start = Math.max(0, this.head - MaxSamples)
+      const end = this.head
+      const height = 50
+      const width = 100
+      let xoff = Math.min(a.pos[0], b.pos[0])
+      const yoff = Math.max(a.pos[0], b.pos[0]) + height + 10
+      const xstep = width / MaxSamples
+
+      ctx.fillStyle = "rgba(255, 255, 255, 0.2)"
+      ctx.strokeStyle = "#fff"
+      ctx.fillRect(xoff, yoff - height, width, height)
+
+      ctx.beginPath()
+      for (let i = start; i < end; i++) {
+        let value = this.ring[i % MaxSamples]
+        ctx.moveTo(xoff, yoff)
+        ctx.lineTo(xoff, yoff - value * (height - 5))
+        xoff += xstep;
+      }
+      ctx.stroke()
+
+    }
+  }
 }
 
 const TAU = Math.PI * 2.0
@@ -63,7 +135,12 @@ function Init(rootElement) {
   })
 
   const receptors = [
-    CreateReceptor(512, 512)
+    CreateReceptor(490, 512),
+    CreateReceptor(530, 512),
+  ]
+
+  const receptorPairs = [
+    CreateReceptorPair(receptors[0], receptors[1])
   ]
 
   const LightRadius = 50
@@ -79,10 +156,15 @@ function Init(rootElement) {
 
     // draw the receptors
     for (let i = 0; i < receptors.length; i++) {
-      const value = Math.max(0, LightRadius - Math.abs(receptors[i].pos[0] - mouse.pos[0])) / LightRadius
+      const value = Math.max(0, LightRadius - Math.abs(receptors[i].pos[0] - mouse.pos[0])) / LightRadius > 0 ? 1.0 : 0.0
       receptors[i].add(value)
       receptors[i].render(ctx);
     }
+
+    receptorPairs.forEach(pair => {
+      pair.tick()
+      pair.render(ctx)
+    })
 
 
 
