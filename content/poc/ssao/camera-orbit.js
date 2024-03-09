1f0b11ef80517611b4a8a2dee8a4db0b34110b85
Elijah Insua <tmpvar@gmail.com>
2024-03-09 09:42:19 -0700

    begin work on ssao poc

---
+++ content/poc/ssao/camera-orbit.js
@@ -0,0 +1,415 @@
+export default function CreateOrbitCamera(canvas) {
+  if (!canvas) {
+    throw new Error("no canvas")
+  }
+
+  const EPSILON = 1e-17
+  const DegreesToRadians = 0.0174532925199
+
+  // extracted from glmatrix
+  // see: https://github.com/toji/gl-matrix/blob/master/src/mat4.js
+  // license: MIT Copyright (c) 2015-2021, Brandon Jones, Colin MacKenzie IV.
+
+  /**
+   * Set a mat4 to the identity matrix
+   *
+   * @param {mat4} out the receiving matrix
+   * @returns {mat4} out
+   */
+  function Identity(out) {
+    out[0] = 1;
+    out[1] = 0;
+    out[2] = 0;
+    out[3] = 0;
+    out[4] = 0;
+    out[5] = 1;
+    out[6] = 0;
+    out[7] = 0;
+    out[8] = 0;
+    out[9] = 0;
+    out[10] = 1;
+    out[11] = 0;
+    out[12] = 0;
+    out[13] = 0;
+    out[14] = 0;
+    out[15] = 1;
+    return out;
+  }
+
+
+  /**
+   * Generates a perspective projection matrix with the given bounds.
+   * The near/far clip planes correspond to a normalized device coordinate Z range of [-1, 1],
+   * which matches WebGL/OpenGL's clip volume.
+   * Passing null/undefined/no value for far will generate infinite projection matrix.
+   *
+   * @param {mat4} out mat4 frustum matrix will be written into
+   * @param {number} fovy Vertical field of view in radians
+   * @param {number} aspect Aspect ratio. typically viewport width/height
+   * @param {number} near Near bound of the frustum
+   * @param {number} far Far bound of the frustum, can be null or Infinity
+   * @returns {mat4} out
+   */
+  function PerspectiveNO(out, fovy, aspect, near, far) {
+    const f = 1.0 / Math.tan(fovy / 2);
+    out[0] = f / aspect;
+    out[1] = 0;
+    out[2] = 0;
+    out[3] = 0;
+    out[4] = 0;
+    out[5] = f;
+    out[6] = 0;
+    out[7] = 0;
+    out[8] = 0;
+    out[9] = 0;
+    out[11] = -1;
+    out[12] = 0;
+    out[13] = 0;
+    out[15] = 0;
+    if (far != null && far !== Infinity) {
+      const nf = 1 / (near - far);
+      out[10] = (far + near) * nf;
+      out[14] = 2 * far * near * nf;
+    } else {
+      out[10] = -1;
+      out[14] = -2 * near;
+    }
+    return out;
+  }
+
+  /**
+   * Generates a look-at matrix with the given eye position, focal point, and up axis.
+   * If you want a matrix that actually makes an object look at another object, you should use targetTo instead.
+   *
+   * @param {mat4} out mat4 frustum matrix will be written into
+   * @param {ReadonlyVec3} eye Position of the viewer
+   * @param {ReadonlyVec3} center Point the viewer is looking at
+   * @param {ReadonlyVec3} up vec3 pointing up
+   * @returns {mat4} out
+   */
+  function LookAt(out, eye, center, up) {
+    let x0, x1, x2, y0, y1, y2, z0, z1, z2, len;
+    let eyex = eye[0];
+    let eyey = eye[1];
+    let eyez = eye[2];
+    let upx = up[0];
+    let upy = up[1];
+    let upz = up[2];
+    let centerx = center[0];
+    let centery = center[1];
+    let centerz = center[2];
+
+    if (
+      Math.abs(eyex - centerx) < EPSILON &&
+      Math.abs(eyey - centery) < EPSILON &&
+      Math.abs(eyez - centerz) < EPSILON
+    ) {
+      return Identity(out);
+    }
+
+    z0 = eyex - centerx;
+    z1 = eyey - centery;
+    z2 = eyez - centerz;
+
+    len = 1 / Math.sqrt(z0 * z0 + z1 * z1 + z2 * z2);
+    z0 *= len;
+    z1 *= len;
+    z2 *= len;
+
+    x0 = upy * z2 - upz * z1;
+    x1 = upz * z0 - upx * z2;
+    x2 = upx * z1 - upy * z0;
+    len = Math.sqrt(x0 * x0 + x1 * x1 + x2 * x2);
+    if (!len) {
+      x0 = 0;
+      x1 = 0;
+      x2 = 0;
+    } else {
+      len = 1 / len;
+      x0 *= len;
+      x1 *= len;
+      x2 *= len;
+    }
+
+    y0 = z1 * x2 - z2 * x1;
+    y1 = z2 * x0 - z0 * x2;
+    y2 = z0 * x1 - z1 * x0;
+
+    len = Math.sqrt(y0 * y0 + y1 * y1 + y2 * y2);
+    if (!len) {
+      y0 = 0;
+      y1 = 0;
+      y2 = 0;
+    } else {
+      len = 1 / len;
+      y0 *= len;
+      y1 *= len;
+      y2 *= len;
+    }
+
+    out[0] = x0;
+    out[1] = y0;
+    out[2] = z0;
+    out[3] = 0;
+    out[4] = x1;
+    out[5] = y1;
+    out[6] = z1;
+    out[7] = 0;
+    out[8] = x2;
+    out[9] = y2;
+    out[10] = z2;
+    out[11] = 0;
+    out[12] = -(x0 * eyex + x1 * eyey + x2 * eyez);
+    out[13] = -(y0 * eyex + y1 * eyey + y2 * eyez);
+    out[14] = -(z0 * eyex + z1 * eyey + z2 * eyez);
+    out[15] = 1;
+
+    return out;
+  }
+
+  /**
+  * Multiplies two mat4s
+  *
+  * @param {mat4} out the receiving matrix
+  * @param {ReadonlyMat4} a the first operand
+  * @param {ReadonlyMat4} b the second operand
+  * @returns {mat4} out
+  */
+  function Multiply(out, a, b) {
+    let a00 = a[0],
+      a01 = a[1],
+      a02 = a[2],
+      a03 = a[3];
+    let a10 = a[4],
+      a11 = a[5],
+      a12 = a[6],
+      a13 = a[7];
+    let a20 = a[8],
+      a21 = a[9],
+      a22 = a[10],
+      a23 = a[11];
+    let a30 = a[12],
+      a31 = a[13],
+      a32 = a[14],
+      a33 = a[15];
+
+    // Cache only the current line of the second matrix
+    let b0 = b[0],
+      b1 = b[1],
+      b2 = b[2],
+      b3 = b[3];
+    out[0] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+    out[1] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+    out[2] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+    out[3] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+
+    b0 = b[4];
+    b1 = b[5];
+    b2 = b[6];
+    b3 = b[7];
+    out[4] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+    out[5] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+    out[6] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+    out[7] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+
+    b0 = b[8];
+    b1 = b[9];
+    b2 = b[10];
+    b3 = b[11];
+    out[8] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+    out[9] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+    out[10] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+    out[11] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+
+    b0 = b[12];
+    b1 = b[13];
+    b2 = b[14];
+    b3 = b[15];
+    out[12] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+    out[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+    out[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+    out[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+    return out;
+  }
+
+  /**
+ * Inverts a mat4
+ *
+ * @param {mat4} out the receiving matrix
+ * @param {ReadonlyMat4} a the source matrix
+ * @returns {mat4} out
+ */
+  function Invert(out, a) {
+    let a00 = a[0],
+      a01 = a[1],
+      a02 = a[2],
+      a03 = a[3];
+    let a10 = a[4],
+      a11 = a[5],
+      a12 = a[6],
+      a13 = a[7];
+    let a20 = a[8],
+      a21 = a[9],
+      a22 = a[10],
+      a23 = a[11];
+    let a30 = a[12],
+      a31 = a[13],
+      a32 = a[14],
+      a33 = a[15];
+
+    let b00 = a00 * a11 - a01 * a10;
+    let b01 = a00 * a12 - a02 * a10;
+    let b02 = a00 * a13 - a03 * a10;
+    let b03 = a01 * a12 - a02 * a11;
+    let b04 = a01 * a13 - a03 * a11;
+    let b05 = a02 * a13 - a03 * a12;
+    let b06 = a20 * a31 - a21 * a30;
+    let b07 = a20 * a32 - a22 * a30;
+    let b08 = a20 * a33 - a23 * a30;
+    let b09 = a21 * a32 - a22 * a31;
+    let b10 = a21 * a33 - a23 * a31;
+    let b11 = a22 * a33 - a23 * a32;
+
+    // Calculate the determinant
+    let det =
+      b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+
+    if (!det) {
+      return null;
+    }
+    det = 1.0 / det;
+
+    out[0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
+    out[1] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
+    out[2] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
+    out[3] = (a22 * b04 - a21 * b05 - a23 * b03) * det;
+    out[4] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
+    out[5] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
+    out[6] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
+    out[7] = (a20 * b05 - a22 * b02 + a23 * b01) * det;
+    out[8] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
+    out[9] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
+    out[10] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
+    out[11] = (a21 * b02 - a20 * b04 - a23 * b00) * det;
+    out[12] = (a11 * b07 - a10 * b09 - a12 * b06) * det;
+    out[13] = (a00 * b09 - a01 * b07 + a02 * b06) * det;
+    out[14] = (a31 * b01 - a30 * b03 - a32 * b00) * det;
+    out[15] = (a20 * b03 - a21 * b01 + a22 * b00) * det;
+
+    return out;
+  }
+
+  const state = {
+    EPS: 1e-5,
+    yaw: 0.0,
+    pitch: Math.PI * 0.5,
+    eye: [0.0, 2.0, 0.0],
+    target: [0.0, 0.0, 0.0],
+    up: [0.0, 1.0, 0.0],
+    distance: 0.5,
+    targetDistance: 0.5,
+    minDistance: 0.01,
+    maxDistance: 10,
+    projection: new Float32Array(16),
+    view: new Float32Array(16),
+    worldToScreen: new Float32Array(16),
+    screenToWorld: new Float32Array(16),
+    sensitivity: 0.005,
+    scrollSensitivity: 0.001,
+    fov: 90 * DegreesToRadians,
+
+    mouse: {
+      pos: [0, 0],
+      lastPos: [0, 0],
+      down: false
+    },
+  }
+
+  const camera = {
+    state: state,
+
+    rotate(dx, dy) {
+      dx *= state.sensitivity
+      dy *= state.sensitivity
+
+      state.yaw += dx
+      state.pitch = Math.max(0.1, Math.min(state.pitch + dy, Math.PI - 0.1))
+    },
+
+    zoom(delta) {
+      state.targetDistance = Math.max(
+        state.minDistance,
+        Math.min(state.targetDistance + delta * state.scrollSensitivity, state.maxDistance)
+      )
+    },
+
+    tick(width, height, deltaTime) {
+      let delta = (state.targetDistance - state.distance) * 9.9999999 * deltaTime
+
+      // if the camera is moving, let the caller know
+      let ret = false
+      if (Math.abs(delta) > 0.01) {
+        state.distance += delta
+        ret = true
+      } else {
+        state.distance = state.targetDistance
+      }
+
+      state.eye[2] = Math.sin(state.pitch) * Math.sin(state.yaw) * state.distance
+      state.eye[0] = Math.sin(state.pitch) * Math.cos(state.yaw) * state.distance
+      state.eye[1] = Math.cos(state.pitch) * state.distance
+
+      LookAt(state.view, state.eye, state.target, state.up)
+      PerspectiveNO(state.projection, state.fov, width / height, state.minDistance, state.maxDistance)
+      Multiply(state.worldToScreen, state.projection, state.view)
+      Invert(state.screenToWorld, state.worldToScreen)
+      return ret
+    }
+  }
+
+  const MoveMouse = (x, y) => {
+    let ratioX = canvas.width / canvas.clientWidth
+    let ratioY = canvas.height / canvas.clientHeight
+    state.mouse.pos[0] = x * ratioX
+    state.mouse.pos[1] = y * ratioY
+  }
+
+  window.addEventListener("mouseup", e => {
+    state.mouse.down = false
+  })
+
+  canvas.addEventListener("mousedown", (e) => {
+    state.mouse.down = true
+    MoveMouse(e.offsetX, e.offsetY);
+    state.mouse.lastPos[0] = state.mouse.pos[0]
+    state.mouse.lastPos[1] = state.mouse.pos[1]
+
+    e.preventDefault()
+  }, { passive: false })
+
+  canvas.addEventListener("mousemove", e => {
+    MoveMouse(e.offsetX, e.offsetY)
+    e.preventDefault()
+
+    if (state.mouse.down) {
+      let dx = state.mouse.pos[0] - state.mouse.lastPos[0]
+      let dy = state.mouse.pos[1] - state.mouse.lastPos[1]
+
+      state.mouse.lastPos[0] = state.mouse.pos[0]
+      state.mouse.lastPos[1] = state.mouse.pos[1]
+
+      if (Math.abs(dx) < 1.0 && Math.abs(dy) < 1.0) {
+        return;
+      }
+
+      camera.rotate(dx, -dy)
+    }
+  }, { passive: false })
+
+  canvas.addEventListener("wheel", e => {
+    camera.zoom(e.deltaY)
+    e.preventDefault()
+  }, { passive: false })
+
+  return camera
+}
